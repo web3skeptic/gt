5890ebffe92086f546092d325e2774026faf1250
web3skeptic <web3skeptic@tuta.com>
2025-04-04 12:11:38 +0200

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import GymTracker from './components/GymTracker';
+
+function App() {
+  return (
+    <div className="App">
+      <GymTracker />
+    </div>
+  );
+}
+
+export default App;
